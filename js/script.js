2f1e9eae5de2d6338bb0fa9b4d66fe76298ca153
boniverski <bosko.rabrenovic@gmail.com>
2017-03-19 17:14:07 +0100

    add tweet button

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
+// Getting quotes data from crossorigin.me APIs:
 function getQuote() {
   $.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json", function(json) {
-
+    //Getting quote content, adding fading effect on change:
     $("#text").fadeOut("fast", function() {
       $(this).html(json.quote);
     }).fadeIn("fast");
 
+    //Getting author's name, adding fading effect on change:
     $(".quote-author").fadeOut("fast", function() {
       $(this).html(json.author);
     }).fadeIn("fast");
   });
 }
 
-$(document).ready(function() {
-  /*getQuote();*/
+function tweet(){
+  var quote = $("#text").text();
+  var author = $(".quote-author").text();
+  window.open("https://twitter.com/intent/tweet?text="+"\""+quote+"\""+"  "+"– "+author);
+}
 
+//Taking care of buttons when page is loaded
+$(document).ready(function() {
+  //getQuote(); <-- Optional: Display quote on page load
   $("#button-quote").on("click", getQuote);
+  $("#button-tweet").on("click", tweet);
 
 });
